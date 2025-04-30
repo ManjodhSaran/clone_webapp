66585508fbd8cc2423f63c0c2ea4ccac8ebdfe08
Manjodhsaran <manjodhsaran1@gmail.com>
2025-05-01 00:01:20 +0530

    fix defult fn

--- src/v1/lib/getUrls.js
+++ src/v1/lib/getUrls.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
-import FormData from 'form-data'; // npm install form-data
-import axios from 'axios';         // npm install axios
+import FormData from 'form-data';
+import axios from 'axios';
 
 const subjects_base_url = ({ curr, currYear }) =>
     `https://www.iblib.com/api/study/subjects?curr=${curr}&currYear=${currYear}`;
@@ -51,11 +51,6 @@ export const uploadToServer = async ({ name, token }) => {
         return null;
     }
 };
-
-uploadToServer({
-    name: 'ICSE_CLASS-IV_Computer Studies',
-    token: 'eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJpYmxpYkpXVCIsInN1YiI6IntcImxvZ2luTmFtZVwiOlwibmVoYS5hcnJvd1wiLFwidXNlclwiOntcImlkXCI6XCIxMDNcIixcImxvZ2luTmFtZVwiOlwibmVoYS5hcnJvd1wiLFwiaWRTdHVkZW50XCI6XCJcIixcImZpcnN0TmFtZVwiOlwiTmVoYVwiLFwibGFzdE5hbWVcIjpcIkFycm93XCIsXCJmYXRoZXJOYW1lXCI6XCJcIixcInBob25lTnVtYmVyXCI6XCI2Mzk1OTUyMjcxXCIsXCJlbWFpbEFkZHJlc3NcIjpcImlibGliLmluZm9AZ21haWwuY29tXCIsXCJiaXJ0aERhdGVcIjpcIjIwMDAtMDEtMDFcIixcImdlbmRlclwiOlwiRkVNQUxFXCIsXCJ1c2VySW1hZ2VcIjpcImh0dHBzOi8vZ3JhZGVwbHVzLnMzLmFwLXNvdXRoLTEuYW1hem9uYXdzLmNvbS91c2Vycy9hc3NldHMvaW1nL3VzZXJzL2Nyb3BwZWQ5MjE0MzgxMjk5MjI3Nzg1ODg1LmpwZ1wiLFwidXNlclR5cGVcIjpcIkNMSUVOVF9BRE1JTlwiLFwiaWRTY2hvb2xcIjpcIjQ4XCIsXCJzY2hvb2xOYW1lXCI6XCJBcnJvdyBJbnRlciBDb2xsZWdlXCIsXCJjdXJyXCI6XCJDQlNFXCIsXCJjdXJyWWVhclwiOlwiQ0xBU1MtWFwiLFwieWVhckdyb3VwXCI6XCJcIixcImlkQWRkcmVzc1wiOlwiMzU5XCIsXCJsb2NhbEFkZHJlc3NcIjpcIlVHRiAwMywgVHJpbml0eSBTcXVhcmVcIixcImlzTG9ja2VkXCI6XCIwXCJ9LFwicm9sZVwiOltdfSIsImF1dGhvcml0aWVzIjpbIlJPTEVfVVNFUiJdLCJpYXQiOjE3NDYwMzcxMzAsImV4cCI6MTc0NjA0NjEzMH0.rK-aC8VL5NDLgMQamMhwPkSNA3WnT77yzzy_uMvIFqdHbX3EJPR2uS5-oG7j1lR9VX0GJwVdMbMYmHK7Ihf2dw'
-})
 
 export const getSubjectsFromRequest = async ({ token, curr, currYear }) => {
     try {
