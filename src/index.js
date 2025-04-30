eba208f2e581c5a7395e40e9614c3a5f90ff3633
Manjodhsaran <manjodhsaran1@gmail.com>
2025-04-30 20:33:28 +0530

    homepage

--- src/index.js
+++ src/index.js
@@ -9,6 +9,11 @@ app.set('view engine', 'ejs');
 
 app.use('/v1/api', router)
 
+// home page
+app.get('/', (req, res) => {
+  res.send('Offline API is running');
+});
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
